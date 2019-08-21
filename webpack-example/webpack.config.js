d5d2b7a88f566f1481ca61b03750565939aa8b42
Edward.Guo <gonelove.guo@gmail.com>
2019-08-22 00:05:14 +0800

    feat: 学习webpack-dev-server

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     mode: "development",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/assets/'
     },
     module: {
         rules: [
